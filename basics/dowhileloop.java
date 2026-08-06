577e083ab6179c7e69a65c7277e6b44aeb227e6c
Avanti <avanti.work01@gmail.com>
2026-08-06 18:28:48 +0530

    Do while loop - basics

---
+++ basics/dowhileloop.java
@@ -0,0 +1,11 @@
+//to print hello world using do whle loop
+public class abc{
+    public static void main(String args[]){
+        int counter = 1;
+
+        do{
+            System.out.println("Hello world <3");
+            counter++;
+        } while(counter<= 10);
+    }
+}
