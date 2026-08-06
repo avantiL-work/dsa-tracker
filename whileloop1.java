0c1b152e6074123b511ab6eb8316f6e298a91f05
Avanti <avanti.work01@gmail.com>
2026-08-06 17:23:56 +0530

    While loop 1 - basics

---
+++ whileloop1.java
@@ -0,0 +1,13 @@
+//to print hello world 10 times using while loop
+public class abc{
+    public static void main(String args[]){
+
+     //the counter was introduced to maintain a count and increase it by 1 in the loop   
+    int counter = 0;
+
+    while(counter < 10){
+        System.out.println("Hello world <3");
+        counter++;
+    }
+    }
+}
