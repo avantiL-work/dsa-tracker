bc8b49c15cb54c7d99df9196f63dbccb800a48d6
Avanti <avanti.work01@gmail.com>
2026-08-06 17:29:37 +0530

    While loop 1 to 100 print - basics

---
+++ basics/Whileloop1to100.java
@@ -0,0 +1,14 @@
+//to print numbers from 1 to 100 using while loop
+public class Whileloop1to100{
+    public static void main(String args[]){
+//because we want to print numbers from 1 to 100 we begin the counter from 1
+        int counter = 1;
+
+        while(counter <= 100){
+            System.out.println(counter);
+            counter++;
+        }
+
+        System.out.println("printed numbers from 1 to 100");
+    }
+}
