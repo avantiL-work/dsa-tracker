2f7d55f3c7d7728945e3c4de756e7a0c7c7cb298
Avanti <avanti.work01@gmail.com>
2026-08-10 22:14:58 +0530

    Basic function code - basics

---
+++ basics/basicfunction.java
@@ -0,0 +1,13 @@
+//code to write without return and without arguement 
+public class basicfunction {
+    public static void HW(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+    }
+
+    public static void main(String args[]){
+        HW();
+    }
+}
