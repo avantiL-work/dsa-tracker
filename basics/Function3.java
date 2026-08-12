ddfef2812a1fd7d56b2dc8aa9d1b39e5d781ec9e
Avanti <avanti.work01@gmail.com>
2026-08-12 10:23:17 +0530

    Function with return with args - basics

---
+++ basics/Function3.java
@@ -0,0 +1,24 @@
+//with return with argument
+import java.util.*;
+
+public class abc{
+
+    public static int sumcalculator(int x , int y){ //formal parameters
+        int sum = x + y;
+        return sum;
+    }
+
+
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter two numbers: ");
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    //out of scope method
+    int sum = sumcalculator(a,b); //arguments or actual parameters
+    System.out.println("the sum is:" + sum);
+
+
+
+}
+}
